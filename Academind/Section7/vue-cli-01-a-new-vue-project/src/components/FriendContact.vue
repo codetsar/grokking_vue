4a6968fca8e871c73e8283f71676f7e856d33840
Yaroslav <yakotsar@yandex.ru>
2021-07-18 01:06:28 +0300

    vue/cli

---
+++ Academind/Section7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <h2 @click="toggleFavorite">{{ name }} {{ isFav ? "(favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    phoneNumber: String,
+    emailAddress: String,
+    isFavorite: Boolean,
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      isFav: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.isFav = !this.isFav;
+    },
+  },
+};
+</script>
