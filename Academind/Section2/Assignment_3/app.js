a715346ba7a170f970e5f3d4143807e2914cd1c4
Yaroslav <yakotsar@yandex.ru>
2021-07-17 03:43:26 +0300

    assignment 3

---
+++ Academind/Section2/Assignment_3/app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      number: 0,
+    };
+  },
+  methods: {
+    add(num) {
+      this.number = this.number + num;
+    },
+  },
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return "Not there yet";
+      } else if (this.number === 37) {
+        return this.number;
+      } else {
+        return "Too much!";
+      }
+    },
+  },
+  watch: {
+    result() {
+	console.log("watching...")
+      const that = this;
+      setTimeout(function () {
+        that.number = 0;
+      }, 5000);
+    },
+  },
+});
+
+app.mount("#assignment");
